00de445d8ff79868db6499116bc9c7f8f06e23aa
Ankit Dimania <ankitd@amazon.com>
2013-12-09 22:25:15 +0530

    Renaming

--- src/com/grootcode/base/sync/SyncHelper.java
+++ src/com/grootcode/base/sync/SyncHelper.java
@@ -4,13 +4,13 @@ import android.accounts.Account;
 import android.content.ContentResolver;
 import android.os.Bundle;
 
-import com.grootcode.base.util.ContractUtils;
+import com.grootcode.android.provider.GrootCodeContractBase;
 
 public class SyncHelper {
 
     public static void requestManualSync(Account mChosenAccount) {
         Bundle b = new Bundle();
         b.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
-        ContentResolver.requestSync(mChosenAccount, ContractUtils.CONTENT_AUTHORITY, b);
+        ContentResolver.requestSync(mChosenAccount, GrootCodeContractBase.CONTENT_AUTHORITY, b);
     }
 }
